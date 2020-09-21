71bdb0a460ed2fdb4eb2195f4b88e21647dbc050
XuJie <414460547@qq.com>
2020-09-21 19:59:13 +0800

    9-21---环境搭建

---
+++ src/main/java/com/beyond233/schedulejoblearn/ScheduleJobLearnApplication.java
@@ -0,0 +1,13 @@
+package com.beyond233.schedulejoblearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleJobLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleJobLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beyond233/schedulejoblearn/ScheduleJobLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond233.schedulejoblearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleJobLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
